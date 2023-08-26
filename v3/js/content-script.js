d9d7392c3976840df6faf5f5145c5d2d3eba762d
L <my917@qq.com>
2023-08-26 16:06:25 +0800

    适配 v3 版本

--- v3/js/content-script.js
+++ v3/js/content-script.js
@@ -3,45 +3,44 @@
 // document.addEventListener("readystatechange",function(e){
 //   console.log("readystatechange:",e);
 // });
-function injectCustomJs(jsPath,callback)
-{
-    jsPath = jsPath || 'js/free-copy.js';
-    var temp = document.createElement('script');
-    temp.setAttribute('type', 'text/javascript');
-    temp.src = chrome.extension.getURL(jsPath);
-    temp.onload = function()
-    {
-        this.parentNode.removeChild(this);
-        callback && callback();
-    };
-    document.documentElement.appendChild(temp);
+function injectCustomJs(jsPath, callback) {
+  jsPath = jsPath || 'js/free-copy.js';
+  var temp = document.createElement('script');
+  temp.setAttribute('type', 'text/javascript');
+  const realJsPath = chrome.runtime.getURL(jsPath);
+  // const realJsPath = "chrome-extension://fgldjannpnbenfpbhdiojfgjfmjkmnpk/js/free-copy.js";
+  temp.src = realJsPath;
+  temp.onload = function () {
+    this.parentNode.removeChild(this);
+    callback && callback();
+  };
+  // console.log(document.documentElement.appendChild)
+  document.documentElement.appendChild(temp);
 }
-injectCustomJs('js/free-copy.js',function(){});
+injectCustomJs('js/free-copy.js', function () { });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse)
-{
-  if(request.cmd == 'catchImg') {
-    window.postMessage({"type": 'catchImg'}, '*');
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.cmd == 'catchImg') {
+    window.postMessage({ "type": 'catchImg' }, '*');
   }
-  if(request.cmd == 'catchAllImg') {
-    window.postMessage({"type": 'catchAllImg'}, '*');
+  if (request.cmd == 'catchAllImg') {
+    window.postMessage({ "type": 'catchAllImg' }, '*');
   }
-  if(request.cmd == 'recover-right-click') {
-    window.postMessage({"type": 'recover-right-click'}, '*');
+  if (request.cmd == 'recover-right-click') {
+    window.postMessage({ "type": 'recover-right-click' }, '*');
   }
   sendResponse('我收到了你的消息！');
 });
 
 
-window.addEventListener("message", function(e)
-{
-  if(e.data.type == "showImg"){
+window.addEventListener("message", function (e) {
+  if (e.data.type == "showImg") {
     showImg(e.data.results);
   }
 }, false);
 
-var showImg = function(results){
-  chrome.runtime.sendMessage({type: 'showImg',results}, function(response) {
+var showImg = function (results) {
+  chrome.runtime.sendMessage({ type: 'showImg', results }, function (response) {
     // response 回复内容
   });
 };
--- v3/js/show-img.js
+++ v3/js/show-img.js
@@ -1,37 +1,46 @@
 var rs = [];
-var insert = function(results){
-  if(typeof results != "undefined" && Array.isArray(results)){
+var insert = function (results) {
+  if (typeof results != "undefined" && Array.isArray(results)) {
     var template = document.querySelector(".template");
-    var panel = document.querySelector(".imgs-panel"); 
-    results.forEach(function(result) {
-      if(rs.findIndex(function(r){ return r.url == result.url }) < 0){
+    var panel = document.querySelector(".imgs-panel");
+    results.forEach(function (result) {
+      if (rs.findIndex(function (r) { return r.url == result.url }) < 0) {
         rs.push(result);
         var link = template.cloneNode(true);
         var img = link.querySelector(".img");
         var type = link.querySelector(".type");
         link.classList.remove("template");
         // 处理svg
-        if(result.tag == "svg"){
-          var file  = new File([result.url],"free-copy.svg",{type:"image/svg+xml;charset=utf-8"});
+        if (result.tag == "svg") {
+          var file = new File([result.url], "free-copy.svg", { type: "image/svg+xml;charset=utf-8" });
           var dataUrl = URL.createObjectURL(file);
           img.src = dataUrl;
           link.href = dataUrl;
           // type.innerHTML = "svg (矢量图)";
-        }else{
+        } else {
           link.href = result.url;
           img.src = result.url;
           // type.innerHTML = "image (位图)";
         }
-        img.addEventListener("error",function(e){
+        img.addEventListener("error", function (e) {
           // console.log("error:",e,img);
           link.remove();
-        },false);
-        if(panel.firstElementChild){
-          panel.insertBefore(link,panel.firstElementChild);
-        }else{
+        }, false);
+        if (panel.firstElementChild) {
+          panel.insertBefore(link, panel.firstElementChild);
+        } else {
           panel.appendChild(link);
         }
       }
     });
   }
 };
+
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  if (request.type === "insertResults") {
+    // console.log('results:', request.results)
+    insert(request.results)
+  }
+  sendResponse('res')
+});
